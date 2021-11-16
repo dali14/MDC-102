c4c3d9df46a46a4c959654354f9c1b23c17a2fbf
DELL <dalijlasii@gmail.com>
2021-11-16 22:39:46 +0100

    Make a card collection

--- lib/home.dart
+++ lib/home.dart
@@ -13,11 +13,46 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import 'model/products_repository.dart';
+import 'model/product.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   // TODO: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+      count,
+          (int index) => Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 18.0 / 11.0,
+              child: Image.asset('assets/diamond.png'),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Title'),
+                  const SizedBox(height: 8.0),
+                  Text('Secondary Text'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+    return cards;
+  }
   // TODO: Add a variable for Category (104)
 
   @override
@@ -63,32 +98,8 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
+        children: _buildGridCards(10)
         // TODO: Build a grid of cards (102)
-    children: <Widget>[
-    Card(
-    clipBehavior: Clip.antiAlias,
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-    AspectRatio(
-    aspectRatio: 18.0 / 11.0,
-    child: Image.asset('assets/diamond.png'),
-    ),
-    Padding(
-    padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-    child: Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-    Text('Title'),
-    const SizedBox(height: 8.0),
-    Text('Secondary Text'),
-    ],
-    ),
-    ),
-    ],
-    ),
-    )
-    ],
       ),
       resizeToAvoidBottomInset: false,
     );
